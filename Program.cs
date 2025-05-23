15358270460e2a6255a7c5884c12ec843331e593
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-23 20:05:36 -0300

    f

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("--- Manteiga ou Margarina? ---\n");
+
+Console.Write("Digite o preço da Manteiga (R$).....: ");
+decimal Manteiga = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o preço da Margarina (R$)...: ");
+decimal Margarina = Convert.ToDecimal(Console.ReadLine());
+
+string recomendacao;
+
+if (ComprarAMargarina(Manteiga, Margarina))
+{
+    recomendacao = "Margarina";
+}
+else
+{
+    recomendacao = "Manteiga";
+}
+
+bool ComprarAMargarina(decimal manteiga, decimal margarina)
+{
+    throw new NotImplementedException();
+}
+
+double razao = RazaoMargarinaManteiga(Manteiga, Margarina) * 100;
+
+int RazaoMargarinaManteiga(decimal manteiga, decimal margarina)
+{
+    throw new NotImplementedException();
+}
+
+Console.WriteLine($"\nO preço da Manteiga corresponde a {razao:N1}% do preço da Margarina.");
+Console.WriteLine($"\nRecomendação: Compre a {recomendacao.ToUpper()}.");
+
+double RazaoManteigaMargarina(decimal precoManteiga, decimal precoMargarina)
+{
+    return Convert.ToDouble(precoManteiga / precoMargarina);
+}
+
+bool ComprarAManteiga(decimal precoManteiga, decimal precoMargarina)
+{
+    const double MinRazaoCompraMargarina = 0.73;
+    return RazaoManteigaMargarina(precoManteiga, precoMargarina) >= MinRazaoCompraMargarina;
+}
